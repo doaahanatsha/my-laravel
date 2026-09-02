46610b4ca9eb0ff5b27273ca25372ca613ee0e64
doaahanatsha <hanatshedoaa@gmail.com>
2026-09-02 19:21:44 +0300

    Complete Task 8 - Finalization

--- database/seeders/Adminseeder.php
+++ database/seeders/Adminseeder.php
@@ -3,22 +3,22 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class Adminseeder extends Seeder
+class AdminSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        User::create([
-    'name' => 'Admin',
-    'email' => 'admin@gmail.com',
-    'password' => Hash::make('12345678'),
-    'role' => 'admin',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'name' => 'Admin',
+                'password' => '12345678',
+                'role' => 'admin',
+            ]
+        );
     }
 }
